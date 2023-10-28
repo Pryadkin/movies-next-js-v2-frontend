a8e10d0848036e06f33298583ca900de92556f3c
Anton Pryadkin <ap.pryadkin@dm-solutions.ru>
2023-10-28 14:02:49 +0700

    update ModelDetails, add settings btn to ModelDetails

--- src/components/ModelDetails/ModelDetails.tsx
+++ src/components/ModelDetails/ModelDetails.tsx
@@ -2,18 +2,24 @@ import React from 'react'
 import {useSelector} from 'react-redux'
 
 import {
+    Button,
     Modal,
 } from 'antd'
-
+import clsx from 'clsx'
 
 import {IDetailsMovie, IMovieLang} from '@/api/apiTypes'
 import {useFetchDetailsMovie} from '@/hooks/useFetchDetailsMovie'
+import {getIsDrawerMovieTagsOpen, setSelectMovie} from '@/redux/reducers'
 import {getSelectLanguage} from '@/redux/selectors/layoutSelectors'
-import {ChartElement} from '@/ui-kit'
 
 import styles from './ModelDetails.module.scss'
 
+import {useAppDispatch} from '@/redux/store'
+import {ChartElement} from '@/ui-kit'
+
+
 import {Spin} from '../Spin'
+
 
 interface Props {
     movie: IMovieLang,
@@ -26,12 +32,18 @@ export const ModelDetails = ({
     isModalOpen,
     onModalCancel,
 }: Props) => {
+    const dispatch = useAppDispatch()
     const lang = useSelector(getSelectLanguage)
 
     const {
         data,
         isFetching
     } = useFetchDetailsMovie(movie?.id, lang, movie?.settings?.isTv)
+
+    const handleFilterBtnClick = (movieId: number) => () => {
+        dispatch(getIsDrawerMovieTagsOpen(true))
+        dispatch(setSelectMovie(movieId))
+    }
 
     const getPage = (movie: IDetailsMovie) => {
         if (movie) {
@@ -41,6 +53,13 @@ export const ModelDetails = ({
                 <>
                     <div className={styles.poster}>
                         <img src={movie.poster_path} alt="poster_path" />
+                        <Button
+                            type="default"
+                            className={clsx(styles.btn, styles.btnFilter)}
+                            onClick={handleFilterBtnClick(movie.id)}
+                        >
+                            SETTINGS
+                        </Button>
                     </div>
                     <div className={styles.content}>
                         <h2>{movie.title}</h2>
@@ -63,96 +82,98 @@ export const ModelDetails = ({
                             ]}
                         />
 
-                        <div className={styles.movieDataElem}>
+                        <div className={styles.info}>
+                            <div className={styles.movieDataElem}>
                             Сеть: {movie?.networks?.map(elem => {
-                                return (
-                                    <span key={elem.name}>{elem.name}</span>
-                                )
-                            })}
-                        </div>
-
-                        <div className={styles.movieDataElem}>
-                            {movie.overview}
-                        </div>
-
-                        {movie.seasons && (
-                            <ol className={styles.movieDataElem}>
-                                Сезоны: {movie.seasons.map(elem => {
                                     return (
-                                        <li
-                                            key={elem.id} className="item">
-                                            <div
-                                                style={{marginLeft: 30, color: 'grey'}}
-                                            >
-
-                                                    Название: {elem.name}
-                                            </div>
-                                            <div
-                                                style={{marginLeft: 30, color: 'grey'}}
-                                            >
-
-                                                    Дата релиза: {elem.air_date}
-                                            </div>
-                                            <div
-                                                style={{marginLeft: 30, color: 'grey'}}
-
-                                            >
-                                                Кол-во эпизодов: {elem.episode_count}
-                                            </div>
-                                            <br />
-                                        </li>
-
+                                        <span key={elem.name}>{elem.name}</span>
                                     )
                                 })}
-                            </ol>
-                        )}
+                            </div>
 
-                        <div className={styles.movieDataElem}>
-                            Дата релиза: {movie.release_date}
-                        </div>
+                            <div className={styles.movieDataElem}>
+                                {movie.overview}
+                            </div>
 
-                        <div className={styles.movieDataElem}>
-                            Страна производитель: {movie.production_countries.map(elem => {
-                                return (
-                                    <span key={elem.iso_3166_1}>{elem.name}</span>
-                                )
-                            })}
-                        </div>
+                            {movie.seasons && (
+                                <ol className={styles.movieDataElem}>
+                                Сезоны: {movie.seasons.map(elem => {
+                                        return (
+                                            <li
+                                                key={elem.id} className="item">
+                                                <div
+                                                    style={{marginLeft: 30, color: 'grey'}}
+                                                >
 
-                        <div className={styles.movieDataElem}>
-                            Доход: {movie.revenue}
-                        </div>
+                                                    Название: {elem.name}
+                                                </div>
+                                                <div
+                                                    style={{marginLeft: 30, color: 'grey'}}
+                                                >
 
-                        <div className={styles.movieDataElem}>
-                            Продолжительность: {movie.runtime} мин
-                        </div>
+                                                    Дата релиза: {elem.air_date}
+                                                </div>
+                                                <div
+                                                    style={{marginLeft: 30, color: 'grey'}}
 
-                        <div className={styles.movieDataElem}>
-                            Бюджет: {movie.budget}
-                        </div>
+                                                >
+                                                Кол-во эпизодов: {elem.episode_count}
+                                                </div>
+                                                <br />
+                                            </li>
 
-                        <div className={styles.movieDataElem}>
-                            Статус: {movie.status}
-                        </div>
+                                        )
+                                    })}
+                                </ol>
+                            )}
 
-                        <div className={styles.movieDataElem}>
-                            Языки перевода: {movie.spoken_languages.map(elem => {
-                                return (
-                                    <div
-                                        style={{marginLeft: 30, color: 'grey'}}
-                                        key={elem.name}>{elem.english_name}</div>
-                                )
-                            })}
-                        </div>
+                            <div className={styles.movieDataElem}>
+                                Дата релиза: {movie.release_date}
+                            </div>
 
-                        <div className={styles.movieDataElem}>
-                            Жанр: {movie.genres.map(elem => {
-                                return (
-                                    <div
-                                        style={{marginLeft: 30, color: 'grey'}}
-                                        key={elem.id}>{elem.name}</div>
-                                )
-                            })}
+                            <div className={styles.movieDataElem}>
+                                Доход: {movie.revenue}
+                            </div>
+
+                            <div className={styles.movieDataElem}>
+                                Страна производитель: {movie.production_countries.map(elem => {
+                                    return (
+                                        <span key={elem.iso_3166_1}>{elem.name}</span>
+                                    )
+                                })}
+                            </div>
+
+                            <div className={styles.movieDataElem}>
+                                Продолжительность: {movie.runtime} мин
+                            </div>
+
+                            <div className={styles.movieDataElem}>
+                                Бюджет: {movie.budget}
+                            </div>
+
+                            <div className={styles.movieDataElem}>
+                                Статус: {movie.status}
+                            </div>
+
+                            <div className={styles.movieDataElem}>
+                                Языки перевода: {movie.spoken_languages.map(elem => {
+                                    return (
+                                        <div
+                                            style={{marginLeft: 30, color: 'grey'}}
+                                            key={elem.name}>{elem.english_name}</div>
+                                    )
+                                })}
+                            </div>
+
+                            <div className={styles.movieDataElem}>
+                                Жанр: {movie.genres.map(elem => {
+                                    return (
+                                        <div
+                                            style={{marginLeft: 30, color: 'grey'}}
+                                            key={elem.id}>{elem.name}</div>
+                                    )
+                                })}
+                            </div>
                         </div>
                     </div>
                 </>
